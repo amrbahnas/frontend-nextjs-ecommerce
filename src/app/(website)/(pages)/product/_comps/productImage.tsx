2a6dfbbe174f84d5a7539fde3af8b772624a10d3
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-10-31 18:52:20 +0300

    hot fix

--- src/app/(website)/(pages)/product/_comps/productImage.tsx
+++ src/app/(website)/(pages)/product/_comps/productImage.tsx
@@ -19,23 +19,25 @@ const ProductImages = ({ images }: { images: string[] }) => {
           className="object-cover rounded-md"
         />
       </div>
-      <div className="flex justify-between gap-4 mt-8">
-        {images.map((item: any, i: number) => (
-          <div
-            className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
-            key={item.id}
-            onClick={() => setIndex(i)}
-          >
-            <NextImage
-              src={item}
-              alt=""
-              fill
-              sizes="30vw"
-              className="object-cover rounded-md"
-            />
-          </div>
-        ))}
-      </div>
+      {images.length > 2 && (
+        <div className="flex justify-between gap-4 mt-8">
+          {images.map((item: any, i: number) => (
+            <div
+              className="w-1/4 h-32 relative gap-4 mt-8 cursor-pointer"
+              key={item.id}
+              onClick={() => setIndex(i)}
+            >
+              <NextImage
+                src={item}
+                alt=""
+                fill
+                sizes="30vw"
+                className="object-cover rounded-md"
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };

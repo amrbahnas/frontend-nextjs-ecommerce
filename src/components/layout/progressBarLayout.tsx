0dc385cf28ba66615bbc960584fc537b02def284
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-11-12 00:26:49 +0200

    hot fix

--- src/components/layout/progressBarLayout.tsx
+++ src/components/layout/progressBarLayout.tsx
@@ -1,17 +1,23 @@
 "use client";
 import React from "react";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
+import useUserStore from "@/store/useUserStore";
 
-const ProgressBarLayout = ({ children }: { children: React.ReactNode }) => {
+const ProgressBarLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const emailVerified = useUserStore((state) => state.user?.emailVerified);
   return (
     <>
       {children}
-      <ProgressBar
-        height="4px"
-        color="#F35C7A"
-        options={{ showSpinner: false }}
-        shallowRouting
-      />
+      {emailVerified && (
+        <ProgressBar
+          height="4px"
+          color="#F35C7A"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      )}
     </>
   );
 };

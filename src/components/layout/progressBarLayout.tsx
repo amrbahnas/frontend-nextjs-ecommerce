0b62fcf48a694b5d24d35817f04654719a34f1de
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-11-12 00:37:10 +0200

    hot fixes

--- src/components/layout/progressBarLayout.tsx
+++ src/components/layout/progressBarLayout.tsx
@@ -1,24 +1,44 @@
 "use client";
-import React from "react";
+import React, { useMemo } from "react";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import useUserStore from "@/store/useUserStore";
+import useAuthStore from "@/store/useAuthStore";
 
 const ProgressBarLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const emailVerified = useUserStore((state) => state.user?.emailVerified);
   const active = useUserStore((state) => state.user?.active);
-  return (
-    <>
-      {children}
-      {emailVerified && active && (
+  const isLogin = useAuthStore((state) => state.isLogin);
+
+  const renderProgressBar = useMemo(() => {
+    if (!isLogin)
+      return (
         <ProgressBar
           height="4px"
           color="#F35C7A"
           options={{ showSpinner: false }}
           shallowRouting
         />
-      )}
+      );
+    if (emailVerified && active) {
+      return (
+        <ProgressBar
+          height="4px"
+          color="#F35C7A"
+          options={{ showSpinner: false }}
+          shallowRouting
+        />
+      );
+    } else {
+      return null;
+    }
+  }, [emailVerified, active, isLogin]);
+
+  return (
+    <>
+      {children}
+      {renderProgressBar}
     </>
   );
 };

40c7d7b8da2486b3b1007cd9f523a19d996f5b88
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-11-17 20:09:24 +0200

    fix style

--- src/components/productCard/productCard.skeleton.tsx
+++ src/components/productCard/productCard.skeleton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "antd";
 const ProductCardSkeleton = () => {
   return (
     <div className=" flex flex-col gap-4  bg-gray-100   overflow-hidden rounded-md  hover:shadow-lg  relative">
-      <div className="relative w-full h-72 overflow-hidden">
+      <div className="relative w-full h-40 md:h-72 overflow-hidden">
         <Skeleton.Image className="!w-full !h-full" />
       </div>
       <div className="p-4">

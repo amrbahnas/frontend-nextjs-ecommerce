9de677975a909618ad9da345635c0f319f7445a6
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-11-28 21:33:07 +0200

    hot fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
-import "./globals.css";
 import ProgressBarLayout from "@/components/layout/progressBarLayout";
 import AntDLayout from "@/components/layout/antDLayout";
 import ReactQueryLayout from "@/components/layout/reactQueryLayout";
 import OnlineStatus from "@/components/layout/onlineStatus";
 import { Toaster } from "react-hot-toast";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 

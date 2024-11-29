8158036ab5558b54246a410bc331b9527df2429b
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-11-29 13:22:26 +0200

    hot fix

--- src/components/ui/noData.tsx
+++ src/components/ui/noData.tsx
@@ -1,15 +1,17 @@
+import useBreakPoints from "@/hooks/global/userBreakPoints";
 import Image from "next/image";
 import React from "react";
 
 const NoData = () => {
+  const lg = useBreakPoints("lg");
   return (
     <div className="flex items-center justify-center w-full h-80">
       <Image
         src="No-data.svg"
         alt="No Data"
         className=" opacity-80"
-        width={400}
-        height={400}
+        width={lg ? 400 : 300}
+        height={lg ? 400 : 300}
       />
     </div>
   );

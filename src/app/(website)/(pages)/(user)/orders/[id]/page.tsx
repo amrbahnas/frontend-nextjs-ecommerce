7cfb928968fb188ccf5250448e648c155cd6e48e
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-12-02 22:33:00 +0200

    order page

--- src/app/(website)/(pages)/(user)/orders/[id]/page.tsx
+++ src/app/(website)/(pages)/(user)/orders/[id]/page.tsx
@@ -2,56 +2,60 @@
 import Link from "next/link";
 import { useGetSpecificOrder } from "../_api/query";
 import { Spin } from "antd";
+import Container from "@/components/container";
 
 const OrderPage = ({ params }: { params: { id: string } }) => {
   const id = params.id;
   const { error, isLoading, isError, order } = useGetSpecificOrder(id);
 
   return (
-    <div className="flex flex-col  items-center justify-center ">
-      <Spin spinning={isLoading}>
-        <div className="shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] px-40 py-20">
-          <h1 className="text-xl">Order Details</h1>
-          <div className="mt-12 flex flex-col gap-6">
-            <div className="">
-              <span className="font-medium">Order Id: </span>
+    <Spin spinning={isLoading}>
+      <Container className="flex flex-col items-center justify-center  py-10">
+        <div className="bg-white shadow-lg rounded-lg px-10 py-8 w-full md:w-2/3">
+          <h1 className="text-2xl font-bold mb-6">Order Details</h1>
+          <div className="mt-6 flex flex-col gap-4">
+            <div className="flex justify-between">
+              <span className="font-medium">Order Id:</span>
               <span>{order._id}</span>
             </div>
-            <div className="">
-              <span className="font-medium">Created At: </span>
+            <div className="flex justify-between">
+              <span className="font-medium">Created At:</span>
               <span>{new Date(order.createdAt).toLocaleString()}</span>
             </div>
-            <div className="">
-              <span className="font-medium">Receiver Name: </span>
+            <div className="flex justify-between">
+              <span className="font-medium">Receiver Name:</span>
               <span>{order.user?.name}</span>
             </div>
-            <div className="">
-              <span className="font-medium">Receiver Email: </span>
+            <div className="flex justify-between">
+              <span className="font-medium">Receiver Email:</span>
               <span>{order.user?.email}</span>
             </div>
-            <div className="">
-              <span className="font-medium">Price: </span>
-              <span>{order.totalOrderPrice}</span>
+            <div className="flex justify-between">
+              <span className="font-bold">Price:</span>
+              <span className=" font-bold">{order.totalOrderPrice}$</span>
             </div>
-            <div className="">
-              <span className="font-medium">Payment Status: </span>
+            <div className="flex justify-between">
+              <span className="font-medium">Payment Status:</span>
               <span>{order.isPaid ? "Paid" : "Not Paid"}</span>
             </div>
-            <div className="">
-              <span className="font-medium">Order Status: </span>
+            <div className="flex justify-between">
+              <span className="font-medium">Order Status:</span>
               <span>{order.isDelivered ? "Delivered" : "Not Delivered"}</span>
             </div>
-            <div className="">
-              <span className="font-medium">Delivery Address: </span>
+            <div className="flex justify-between">
+              <span className="font-medium">Delivery Address:</span>
               <span>{order?.address || "No address provided"}</span>
             </div>
           </div>
         </div>
-      </Spin>
-      <Link href="/" className="underline mt-6">
-        Have a problem? Contact us
-      </Link>
-    </div>
+        <Link
+          href="/contact-us"
+          className="underline mt-6 text-blue-500 hover:text-blue-700"
+        >
+          Have a problem? Contact us
+        </Link>
+      </Container>
+    </Spin>
   );
 };
 

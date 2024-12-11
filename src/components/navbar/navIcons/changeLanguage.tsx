06045c80b3fe2f8c9cd53af1d75156b2fd09e501
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-12-11 19:04:06 +0200

    hot fix

--- src/components/navbar/navIcons/changeLanguage.tsx
+++ src/components/navbar/navIcons/changeLanguage.tsx
@@ -1,31 +1,31 @@
-'use client';
+"use client";
 
-import { useParams, usePathname, useRouter } from 'next/navigation';
-import { Button } from 'antd';
+import { useParams, usePathname, useRouter } from "next/navigation";
+import { Button } from "antd";
 import { IoLanguage } from "react-icons/io5";
 
 const ChangeLanguage = () => {
   const router = useRouter();
   const pathname = usePathname();
   const params = useParams();
-  const currentLocale = params?.locale as string || 'en';
+  const currentLocale = (params?.locale as string) || "en";
 
   const toggleLanguage = () => {
-    const newLocale = currentLocale === 'en' ? 'ar' : 'en';
+    const newLocale = currentLocale === "en" ? "ar" : "en";
     // Remove the current locale from the pathname
-    const newPathname = pathname.replace(`/${currentLocale}`, '');
+    const newPathname = pathname.replace(`/${currentLocale}`, "");
     // Add the new locale
     router.push(`/${newLocale}${newPathname}`);
   };
 
   return (
-    <Button 
+    <Button
       type="text"
       onClick={toggleLanguage}
-      className="flex items-center justify-center gap-1"
-      icon={<IoLanguage className="text-lg" />}
+      className="flex items-center justify-center gap-1 w-fit !m-0 !p-0"
+      // icon={<IoLanguage className="text-lg" />}
     >
-      {currentLocale === 'en' ? 'العربية' : 'English'}
+      {currentLocale === "en" ? "Arabic" : "English"}
     </Button>
   );
 };

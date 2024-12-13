ef52254ef87f3bb3f4524f5c34646931af8c10ea
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2024-12-13 16:08:29 +0200

    hot fix

--- src/components/productCard/sections/prImage.tsx
+++ src/components/productCard/sections/prImage.tsx
@@ -3,13 +3,13 @@ import React from "react";
 
 const PrImage = ({ imageCover }: { imageCover: string }) => {
   return (
-    <div className="relative w-full h-40 md:h-72 overflow-hidden ">
+    <div className="relative w-full h-40 md:h-60 overflow-hidden ">
       <NextImage
         src={imageCover || "/product.png"}
         alt=""
         fill
         sizes="25vw"
-        className=" object-cover  md:object-cover rounded-lg "
+        className=" object-contain rounded-lg "
         priority // for avoid Largest Contentful Paint (LCP) issue
       />
     </div>

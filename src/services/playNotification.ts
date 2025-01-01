9b548d6a68ecd8536b2bc78bf71f89b1a6ded22f
Amr elbahnsawty <eng.elbahnsawy@gmail.com>
2025-01-01 18:58:34 +0200

    hot fix

--- src/services/playNotification.ts
+++ src/services/playNotification.ts
@@ -1,5 +1,7 @@
 const notificationSound = "/assets/notification.mp3";
 
+//The audio doesn't work initially because browsers have a policy that requires user interaction before playing audio.
+//The notification sound should work after the user's first interaction with  site
 export const playNotification = () => {
   const audio = new Audio(notificationSound);
   audio.play();

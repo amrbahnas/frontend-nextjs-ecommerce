792ee4465b961e972ffc8f7a12771c4ed358beb6
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2025-07-03 11:02:35 -0700

    fix respose

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,37 +1,33 @@
 "use client";
 import { usePathname } from "next/navigation";
+import { memo } from "react";
 import Container from "../ui/container";
 import DisableLink from "../ui/disableLink";
 import NextImage from "../ui/nextImage";
-import Menu from "./menu";
 import NavIcons from "./navIcons";
-import { PageLinks } from "./pageLinks";
 import SearchBar from "./searchBar";
-import useBreakPoints from "@/hooks/global/userBreakPoints";
-import { memo } from "react";
 
 const Navbar = () => {
   const pathName = usePathname();
-  const lg = useBreakPoints("lg");
 
   return (
     <Container className=" shadow-sm">
-      <div className="min-h-20 flex  flex-col  justify-center py-2 md:py-4 ">
+      <div className="min-h-20 flex  flex-col  justify-center py-2 lg:py-4 ">
         <div className="flex items-center justify-between h-full ">
           {/* left */}
           <DisableLink
             disabled={pathName === "/verifyEmail"}
             href={"/"}
-            className="flex items-center gap-1 md:gap-3 "
+            className="flex items-center gap-1 lg:gap-3 "
           >
             <NextImage src="/logo.png" width={24} height={24} alt="logo" />
-            <div className=" text-lg md:text-2xl tracking-wide">Shope</div>
+            <div className=" text-lg lg:text-2xl tracking-wide">Shope</div>
           </DisableLink>
 
           {/* Big screen */}
           {/* <PageLinks /> */}
           <div>
-            <div className=" hidden md:flex items-center   gap-8 ">
+            <div className=" hidden lg:flex items-center   gap-8 ">
               {/* search input with class hidden will represent at html tree to avoid two input search with same id i added customId prop*/}
               <SearchBar customId="largeId" />
               <NavIcons />
@@ -39,7 +35,7 @@ const Navbar = () => {
             {/* Mobile */}
             <div className="flex items-center gap-6">
               {/* Mobile */}
-              <div className="flex md:hidden items-center justify-between h-full gap-3">
+              <div className="flex lg:hidden items-center justify-between h-full gap-3">
                 <NavIcons />
                 {/* <Menu /> */}
               </div>
@@ -47,7 +43,7 @@ const Navbar = () => {
           </div>
         </div>
         {/*  mobile */}
-        <div className=" mt-3 block md:hidden ">
+        <div className=" mt-3 block lg:hidden ">
           <SearchBar customId="smallId" />
         </div>
       </div>

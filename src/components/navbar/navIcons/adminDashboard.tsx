c629f05e6d971d288ba802626edc6f557f004b79
Amr Elbahnsawy <eng.elbahnsawy@gmail.com>
2025-08-09 13:49:46 -0700

    Implement localization for Admin Dashboard link by integrating translation support for the "Admin Panel" label in both English and Arabic. This enhances accessibility and user experience for admin users.

--- src/components/navbar/navIcons/adminDashboard.tsx
+++ src/components/navbar/navIcons/adminDashboard.tsx
@@ -1,9 +1,11 @@
 import { SmartLink } from "@/components/ui/smartLink";
 import useAuthStore from "@/store/useAuthStore";
+import { useTranslations } from "next-intl";
 import { MdDashboard } from "react-icons/md";
 
 const AdminDashboard = () => {
   const { isAdmin } = useAuthStore();
+  const t = useTranslations("Common");
   if (!isAdmin) return null;
   return (
     <SmartLink
@@ -12,7 +14,7 @@ const AdminDashboard = () => {
       className="flex items-center gap-1 "
     >
       <MdDashboard size={20} />
-      <span className="hidden sm:block">Admin Panel</span>
+      <span className="hidden sm:block">{t("adminPanel")}</span>
     </SmartLink>
   );
 };
